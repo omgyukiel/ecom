654995222cb1b30f49ef74e105aac5baf7f9b130
kennylevu <kennylevu1@gmail.com>
2025-09-09 00:04:11 -0700

    feat(tailwind): add hover effect on button

--- multitenant-ecom/src/app/page.tsx
+++ multitenant-ecom/src/app/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Progress } from "@radix-ui/react-progress";
+import { Progress } from "@/components/ui/progress";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Home() {
   return (
     <div className="p-4">
       <div className="flex flex-col gap-y-4">
         <div>
-          <Button>
+          <Button variant="elevated">
             I am a button
           </Button>
         </div>
