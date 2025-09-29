425aeb7d99e76cbe782df82d5a434e26f79011d7
kennylevu <kennylevu1@gmail.com>
2025-09-29 01:10:10 -0700

    feat(nav): add initial pages to nav

--- multitenant-ecom/src/app/(home)/navbar.tsx
+++ multitenant-ecom/src/app/(home)/navbar.tsx
@@ -1,6 +1,7 @@
+"use client"
 import Link from "next/link";
 import { Poppins } from "next/font/google";
-
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
@@ -22,13 +23,14 @@ const NavbarItem = ({
 }: NavbarItemProps) => {
   return (
     <Button
+      asChild
       variant="outline"
       className={cn(
         "bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg",
         isActive && "bg-black text-white hover:bg-black hover:text-white",
       )}
     >
-      {children}
+      <Link href={href}>{children}</Link>
     </Button>
   )
 }
@@ -42,6 +44,7 @@ const navbarItems = [
 ];
 
 export const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
@@ -55,6 +58,7 @@ export const Navbar = () => {
           <NavbarItem
             key={item.href}
             href={item.href}
+            isActive={pathname == item.href}
           >
             {item.children}
           </NavbarItem>
